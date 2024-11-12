ad5ed61e9862914603221fa6c2d9572ec5bed41e
joelnberns <96081252+joelnberns@users.noreply.github.com>
2024-11-12 14:07:42 -0500

    Add files via upload

--- Boxing_Bop_It.ino
+++ Boxing_Bop_It.ino
@@ -1,12 +1,61 @@
 #include "globals.h"
-#include<Wire.h>
+#include "SoftwareSerial.h"
+#include "DFRobotDFPlayerMini.h"
 
+void setup() {
+  // put your setup code here, to run once:
+  random(analogRead(4));
+  pinMode(blockPin, INPUT);
+  pinMode(LEDGreenPin, OUTPUT);
+  pinMode(LEDRedPin, OUTPUT);
+  pinMode(pressurePin, INPUT);
+  pinMode(accelPinZ, INPUT);
+  pinMode(accelPinY, INPUT);
+  pinMode(A3, INPUT);
+  pinMode(displayPinA0, OUTPUT);
+  pinMode(displayPinA1, OUTPUT);
+  pinMode(displayPinA2, OUTPUT);
+  pinMode(displayPinA3, OUTPUT);
+  pinMode(displayPinB0, OUTPUT);
+  pinMode(displayPinB1, OUTPUT);
+  pinMode(displayPinB2, OUTPUT);
+  pinMode(displayPinB3, OUTPUT);
+
+    // Serial communication with the module
+  #if (defined ESP32)
+    FPSerial.begin(9600, SERIAL_8N1, /*rx =*/D3, /*tx =*/D2);
+  #else
+    FPSerial.begin(9600);
+  #endif
+  mySoftwareSerial.begin(9600);
+  // Initialize Arduino serial
+  // Check if the module is responding and if the SD card is found
+
+  if (!myDFPlayer.begin(mySoftwareSerial)) {
+    digitalWrite(LEDGreenPin, HIGH);
+    while (true);
+  }
+  // Initial settings
+  myDFPlayer.setTimeOut(500);  // Serial timeout 500ms
+  myDFPlayer.volume(20);        // Volume 5
+  myDFPlayer.EQ(0);            // Normal equalization
+  myDFPlayer.outputDevice(DFPLAYER_DEVICE_SD);
+  //  myDFPlayer.EQ(DFPLAYER_EQ_POP);
+  //  myDFPlayer.EQ(DFPLAYER_EQ_ROCK);
+  //  myDFPlayer.EQ(DFPLAYER_EQ_JAZZ);
+  //  myDFPlayer.EQ(DFPLAYER_EQ_CLASSIC);
+  //  myDFPlayer.EQ(DFPLAYER_EQ_BASS);
+
+}
 
 void loop() {
-  // Audio test
-  issueCommand(1);
+  //Audio test
+  //setVolume(15);
+  digitalWrite(LEDRedPin, HIGH);
   delay(1000);
-  issueCommand(2);
+  myDFPlayer.play(1);
+  delay(1000);
+  digitalWrite(LEDRedPin, LOW);
   delay(1000);
 
   /*
@@ -20,16 +69,19 @@ void loop() {
     delay(1000);
     digitalWrite(LEDRedPin, LOW);
   }
+  */
   
   // accelerometer test
     float zAccel = analogRead(accelPinZ);
-    float xAccel = analogRead(accelPinY);
-    float yAccel = analogRead(accelPinX);
-  if (xAccel > 0.1) {
-    digitalWrite(LEDRedPin, HIGH);
-    delay(1000);
+    float xAccel = analogRead(accelPinX);
+    float yAccel = analogRead(accelPinY);
+    if (xAccel > 350) {
+      digitalWrite(LEDRedPin, HIGH);
+      delay(1000);
+    }
     digitalWrite(LEDRedPin, LOW);
-  }
+  /*
+  
 
   // put your main code here, to run repeatedly:
   score = 0;
--- audio.ino
+++ audio.ino
@@ -3,9 +3,11 @@
 #include "DFRobotDFPlayerMini.h"
 
 // Initialize software serial on pins 10 and 11
-SoftwareSerial mySoftwareSerial(DFPlayerRXPin, DFPlayerTXPin);  // RX, TX
-DFRobotDFPlayerMini myDFPlayer;
 
 void issueCommand(int command) {
   myDFPlayer.play(command);
+}
+
+void setVolume(int volume) {
+  myDFPlayer.volume(volume);
 }
